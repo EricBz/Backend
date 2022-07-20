8d2c6728ca8a6d049453bf9dbdab1404fef5ba06
Eric Benitez <99042388+EricBz@users.noreply.github.com>
2022-07-20 14:37:54 -0300

    Carpeta eliminada.

--- Desafio 8/SQLite3.js
+++
@@ -1,9 +0,0 @@
-const knexSqLite = require('knex')({
-    client: 'sqlite3',
-    connection: {
-        filename: "./DB/ecommerce"
-    },
-    useNullAsDefault: true
-  })
-  
-  module.exports = { knexSqLite };
